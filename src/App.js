24a48ad5155e780c32ccc6b35728f40172231567
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-14 15:47:37 -0700

    Added initial styles and initial content.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <UserInput/>
+        <UserOutput/>
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserOutput.css';
+
+const UserOutput = () => {
+  return (
+    <div className="UserOutput">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo porro itaque officiis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nulla.</p>
+    </div>
+  )
+}
+
+export default UserOutput;
